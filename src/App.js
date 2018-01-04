bdcb619e04583159e772d110c5ebe38dd328eaff
kukiat <kukiat1996@gmail.com>
2018-01-04 16:04:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="columns">
+          <div class="column is-7">Auto</div>
+          <div class="column is-5">Auto</div>
+        </div>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
