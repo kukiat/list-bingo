b1a4cb5bb7f65836d132016c7f10e201f8388b32
kukiat <kukiat1996@gmail.com>
2018-01-14 00:18:28 +0700

    fix date

--- src/container/Page.js
+++ src/container/Page.js
@@ -141,29 +141,29 @@ class Page extends Component {
       day+=7
       if(month==1 || month==3 || month== 5 || month== 7 || month==8 || month==10 || month==12) {
         if(day >31) {
-          day=-31
-          month=+1
+          day-=31
+          month+=1
         }
       }else if(month==4 || month==6 || month==9 || month==11){
         if(day >30) {
-          day=-30
-          month=+1
+          day-=30
+          month+=1
         }
       }else {
         if(year == 2012 || year==2016){
           if(day>29) {
-            day=-29
-            month=+1
+            day-=29
+            month+=1
           }
         }else{
           if(day>28) {
-            day=-28
-            month=+1
+            day-=28
+            month+=1
           }
         }
       }
       if(month>12){
-        month+= 1
+        month=1
         year+=1
       }
     })
